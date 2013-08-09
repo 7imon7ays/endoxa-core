5cd281cab7080e3c01ca295bfb1c3a62cee5ef5f
Joe Nelson <cred+github@begriffs.com>
2013-08-09 12:38:05 -0700

    boilerplate

---
+++ test/existence.js
@@ -0,0 +1,9 @@
+var EndoxaCore = require('../module'),
+  chai       = require('chai'),
+  expect     = chai.expect;
+
+describe('EndoxaCore', function() {
+  it('exists', function() {
+    expect(EndoxaCore).to.be.an('object');
+  });
+});
